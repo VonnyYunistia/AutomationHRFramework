9c277afad11ba15413dbadec293f1d53fc8ec53d
vyunistia <vonny.yunistia@gmail.com>
2023-07-07 01:01:22 -0400

    first commit

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,27 @@
+package pages;
+
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.CommonMethods;
+
+public class LoginPage extends CommonMethods {
+    @FindBy(xpath="//*[@id='txtUsername']")
+    public WebElement usernameField;
+
+    @FindBy(id="txtPassword")
+    public WebElement passwordField;
+
+    @FindBy(id="btnLogin")
+    public WebElement loginButton;
+    public LoginPage(){
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(id = "spanMessage")
+    public WebElement errorMsgField;
+
+
+}
